8c34cb7ee152909dcda70dee63bac63c871f99c7
auliaadila <auliaadila36@gmail.com>
2025-02-25 01:39:03 +0000

    Modify pesq and stoi calculation to include csv savings

--- utils/compute_pesq.py
+++ utils/compute_pesq.py
@@ -7,6 +7,7 @@ import scipy.signal as signal
 from pesq import pesq
 from scipy.io import wavfile
 from tqdm import tqdm
+import pandas as pd
 
 def cal_pesq_audio(ref_signal, ref_rate, deg_signal, deg_rate):
     if ref_rate != 16000:
@@ -23,11 +24,14 @@ def cal_pesq_audio(ref_signal, ref_rate, deg_signal, deg_rate):
 
     return nb, wb
 
-def cal_pesq(ref_dir, deg_dir):
+def cal_pesq(ref_dir, deg_dir, csv):
     input_files = glob.glob(f"{deg_dir}/*.wav")
 
     nb_pesq_scores = 0.0
     wb_pesq_scores = 0.0
+
+    results = []
+
     for deg_wav in tqdm(input_files):
         ref_wav = os.path.join(ref_dir, os.path.basename(deg_wav))
         ref_rate, ref = wavfile.read(ref_wav)
@@ -41,8 +45,33 @@ def cal_pesq(ref_dir, deg_dir):
         ref = ref[:min_len]
         deg = deg[:min_len]
 
-        nb_pesq_scores += pesq(16000, ref, deg, 'nb')
-        wb_pesq_scores += pesq(16000, ref, deg, 'wb')
+        nb_pesq = pesq(16000, ref, deg, 'nb')
+        wb_pesq = pesq(16000, ref, deg, 'wb')
+
+        if(csv):
+             # Prepare result dictionary
+            result = {
+                "filename": ref_wav,
+                "pesq_nb": round(nb_pesq, 3),
+                "pesq_wb": round(wb_pesq, 3)
+            }
+            results.append(result)
+
+        nb_pesq_scores += nb_pesq
+        wb_pesq_scores += wb_pesq
+
+    if(csv):
+        # Ensure the directory exists before saving the CSV
+        csv_dir = os.path.dirname(csv)
+        if csv_dir and not os.path.exists(csv_dir):
+            os.makedirs(csv_dir, exist_ok=True)
+            print(f"Created directory: {csv_dir}")
+
+        # Save results to CSV
+        df = pd.DataFrame(results)
+        df.to_csv(csv, index=False)
+        print(f"PESQ results saved to: {csv}")
+
 
     return nb_pesq_scores / len(input_files), wb_pesq_scores / len(input_files)
 
@@ -55,9 +84,26 @@ if __name__ == '__main__':
         '-r', '--ref_dir', required=True, help="Reference wave folder.")
     parser.add_argument(
         '-d', '--deg_dir', required=True, help="Degraded wave folder.")
+    parser.add_argument('-s', '--csv', required=True, help="Saved CSV file path")
 
     args = parser.parse_args()
 
-    nb_score, wb_score = cal_pesq(args.ref_dir, args.deg_dir)
+    nb_score, wb_score = cal_pesq(args.ref_dir, args.deg_dir, args.csv)
     print(f"NB PESQ: {nb_score}")
     print(f"WB PESQ: {wb_score}")
+
+## Results
+# hifi-clean-2
+# python compute_pesq.py -r /workspace/AcademiCodec/dataset/LibriTTS/test-clean-2 -d /workspace/AcademiCodec/egs/HiFi-Codec-24k-240d/output/hf -s /workspace/AcademiCodec/egs/HiFi-Codec-24k-240d/results/pesq-hifi-clean-2.csv
+# NB PESQ: 3.9291073006552617
+# WB PESQ: 3.73306757372778
+
+# hifi-wavmark-clean-2
+# python compute_pesq.py -r /workspace/AcademiCodec/dataset/LibriTTS/test-clean-2 -d /workspace/wavmark/output/hifi-wavmark-clean-2 -s /workspace/wavmark/results/pesq-hifi-wavmark-clean-2.csv
+# NB PESQ: 1.683132453279181
+# WB PESQ: 1.506200950879317
+
+# wavmark-clean-2
+# python compute_pesq.py -r /workspace/AcademiCodec/dataset/LibriTTS/test-clean-2 -d /workspace/wavmark/output/wavmark-clean-2 -s /workspace/wavmark/results/pesq-wavmark-clean-2.csv
+# NB PESQ: 1.761982119127071
+# WB PESQ: 1.6130516998497122
--- utils/compute_stoi.py
+++ utils/compute_stoi.py
@@ -7,6 +7,7 @@ import numpy as np
 from pystoi import stoi
 from scipy.io import wavfile
 from tqdm import tqdm
+import pandas as pd
 
 
 def cal_stoi_audio(ref_signal, deg_signal, sample_rate):
@@ -17,12 +18,15 @@ def cal_stoi_audio(ref_signal, deg_signal, sample_rate):
 
     return cur_stoi
 
-def calculate_stoi(ref_dir, deg_dir):
+def cal_stoi(ref_dir, deg_dir, csv):
     input_files = glob.glob(f"{deg_dir}/*.wav")
+
     if len(input_files) < 1:
         raise RuntimeError(f"Found no wavs in {ref_dir}")
 
+    results = []
     stoi_scores = []
+
     for deg_wav in tqdm(input_files):
         ref_wav = os.path.join(ref_dir, os.path.basename(deg_wav))
         rate, ref = wavfile.read(ref_wav)
@@ -31,7 +35,28 @@ def calculate_stoi(ref_dir, deg_dir):
         ref = ref[:min_len]
         deg = deg[:min_len]
         cur_stoi = stoi(ref, deg, rate, extended=False)
+        
+        if(csv):
+             # Prepare result dictionary
+            result = {
+                "filename": ref_wav,
+                "stoi": round(cur_stoi, 3)
+            }
+            results.append(result)
+
         stoi_scores.append(cur_stoi)
+    
+    if(csv):
+        # Ensure the directory exists before saving the CSV
+        csv_dir = os.path.dirname(csv)
+        if csv_dir and not os.path.exists(csv_dir):
+            os.makedirs(csv_dir, exist_ok=True)
+            print(f"Created directory: {csv_dir}")
+
+        # Save results to CSV
+        df = pd.DataFrame(results)
+        df.to_csv(csv, index=False)
+        print(f"STOI results saved to: {csv}")
 
     return np.mean(stoi_scores)
 
@@ -44,8 +69,28 @@ if __name__ == '__main__':
         '-r', '--ref_dir', required=True, help="Reference wave folder.")
     parser.add_argument(
         '-d', '--deg_dir', required=True, help="Degraded wave folder.")
+    parser.add_argument('-s', '--csv', required=True, help="Saved CSV file path")
 
     args = parser.parse_args()
 
-    stoi_score = calculate_stoi(args.ref_dir, args.deg_dir)
+    stoi_score = cal_stoi(args.ref_dir, args.deg_dir, args.csv)
     print(f"STOI: {stoi_score}")
+
+## Results
+# hifi-clean-2
+# python compute_stoi.py -r /workspace/AcademiCodec/dataset/LibriTTS/test-clean-2 -d /workspace/AcademiCodec/egs/HiFi-Codec-24k-240d/output/hf -s /workspace/AcademiCodec/egs/HiFi-Codec-24k-240d/results/stoi-hifi-clean-2.csv
+# STOI: 0.9380094691230352
+# NB PESQ: 3.9291073006552617
+# WB PESQ: 3.73306757372778
+
+# hifi-wavmark-clean-2
+# python compute_stoi.py -r /workspace/AcademiCodec/dataset/LibriTTS/test-clean-2 -d /workspace/wavmark/output/hifi-wavmark-clean-2 -s /workspace/wavmark/results/stoi-hifi-wavmark-clean-2.csv
+# STOI: 0.9429923506524662
+# NB PESQ: 1.683132453279181
+# WB PESQ: 1.506200950879317
+
+# wavmark-clean-2
+# python compute_stoi.py -r /workspace/AcademiCodec/dataset/LibriTTS/test-clean-2 -d /workspace/wavmark/output/wavmark-clean-2 -s /workspace/wavmark/results/stoi-wavmark-clean-2.csv
+# STOI: 0.9992303874943328
+# NB PESQ: 1.761982119127071
+# WB PESQ: 1.6130516998497122
